03443cabec274ba792e67f19a3dfa06706a71d78
shafwan <iwan28894@gmail.com>
2026-04-29 12:31:59 +0700

    update mobile UI and fix contact section

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,8 @@ export default function RootLayout({
 }>) {
   return (
    
-<html
-  lang="en"
-  className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
->
-  <body className="bg-white text-black dark:bg-gray-900 dark:text-white">
-    {children}
+<html lang="en" className="light">
+  <body className="min-h-full flex flex-col bg-white text-black">
   </body>
 </html>
   );
